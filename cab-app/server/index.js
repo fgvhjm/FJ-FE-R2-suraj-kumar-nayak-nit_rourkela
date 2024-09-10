a032abee031c96a4c38289ffbf1b1b5a10cda6dd
fgvhjm <surajnie123@gmail.com>
2024-09-10 18:54:28 +0530

    anotherurlchnage

--- cab-app/server/index.js
+++ cab-app/server/index.js
@@ -69,7 +69,7 @@ app.post('/payment', async (req, res) => {
 // Set up Socket.IO
 const io = new Server(server, {
   cors: {
-    origin: process.env.FRONTEND_URL || 'https://cab-pygy.vercel.app/payment',
+    origin: process.env.FRONTEND_URL || 'https://cab-pygy.vercel.app',
     methods: ['GET', 'POST'],
   },
 });
