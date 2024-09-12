b171018e51620005edf8c88786ca7fc3f302cfc2
fgvhjm <surajnie123@gmail.com>
2024-09-12 11:06:00 +0530

    sss

--- cab-app/src/chatApp.js
+++ cab-app/src/chatApp.js
@@ -8,7 +8,7 @@ const ChatApp = () => {
   const [socket, setSocket] = useState(null);
 
   useEffect(() => {
-    const socketIo = io('http://localhost:3000');
+    const socketIo = io('https://cab-r852.vercel.app/');
     setSocket(socketIo);
 
     // Listen for messages from the server
